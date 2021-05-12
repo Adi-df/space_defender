56b1355084f4e59e4463f92962074df70f35a568
Adi-df <adidf-web@laposte.net>
2021-05-12 13:01:12 +0200

    Add Deref Trait

--- src/systems/map_renderer.rs
+++ src/systems/map_renderer.rs
@@ -1,7 +1,9 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Deref};
 
 use hecs::World;
 use macroquad::prelude::Color;
+
+use super::physics::{Position, Size};
 
 pub struct Map(pub Vec<String>);
 pub struct MapColor(pub HashMap<char, Color>);
@@ -46,5 +48,21 @@ impl From<Vec<(char, Color)>> for MapColor {
 impl From<Vec<&str>> for Map {
     fn from(vec: Vec<&str>) -> Self {
         Self(vec.into_iter().map(|l| l.to_owned()).collect())
+    }
+}
+
+// Deref Traits
+impl Deref for MapColor {
+    type Target = HashMap<char, Color>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl Deref for Map {
+    type Target = Vec<String>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }

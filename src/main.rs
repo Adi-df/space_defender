5ba5dd679f68d41940b077b08ce726e1fad35904
Adi-df <adidf-web@laposte.net>
2021-05-12 10:12:14 +0200

    Add mainmenu

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use macroquad::prelude::*;
 
+mod mainmenu;
+mod exit_modes;
+
+use crate::exit_modes::ExitMode;
+
 #[macroquad::main("Space Defender")]
 async fn main() {
-    loop {
-        next_frame().await;
-    }
+    if  mainmenu::menu().await == ExitMode::Quit { return; };
 }

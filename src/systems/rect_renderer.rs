2847d99b8ac1d4c994cac243436f6be302c2214b
Adi-df <adidf-web@laposte.net>
2021-05-12 12:25:57 +0200

    Create Rect Renderer system + field public

--- src/systems/rect_renderer.rs
+++ src/systems/rect_renderer.rs
@@ -1,9 +1,18 @@
-use macroquad::prelude::Color;
+use hecs::World;
+use macroquad::prelude::{Color, draw_rectangle};
 
-struct RectRenderer(Color);
+use super::physics::{Position, Size};
+
+pub struct RectRenderer(pub Color);
 
 impl RectRenderer {
     pub fn new(color: Color) -> Self {
         Self(color)
+    }
+}
+
+pub fn rect_renderer_system(world: &mut World) {
+    for (_, (renderer, pos, size)) in world.query_mut::<(&RectRenderer, &Position, &Size)>() {
+        draw_rectangle(pos.0, pos.1, size.0, size.1, renderer.0);
     }
 }

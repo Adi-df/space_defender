96953051f68f0fdf7efed75dbd3dfd1379346760
Adi-df <adidf-web@laposte.net>
2021-05-12 10:24:00 +0200

    Loop game until player quit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use macroquad::prelude::*;
-
 mod mainmenu;
 mod exit_modes;
+mod game;
 
 use crate::exit_modes::ExitMode;
 
 #[macroquad::main("Space Defender")]
 async fn main() {
     if  mainmenu::menu().await == ExitMode::Quit { return; };
+
+    loop {
+        if game::game().await == ExitMode::Quit { break; }
+    }
 }

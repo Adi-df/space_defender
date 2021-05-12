f0d86cf162925e038c8f840832f9c5d1e2c01b17
Adi-df <adidf-web@laposte.net>
2021-05-12 12:03:02 +0200

    Add game loop

--- src/game.rs
+++ src/game.rs
@@ -1,9 +1,19 @@
 use hecs::World;
+use macroquad::prelude::*;
 
 use crate::exit_modes::ExitMode;
+use crate::systems::physics;
 
 pub async fn game() -> ExitMode {
     let mut world = World::new();
+
+    loop {
+        if is_key_pressed(KeyCode::Escape) {
+            break;
+        }
+
+        next_frame().await;
+    }
 
     ExitMode::Quit
 }

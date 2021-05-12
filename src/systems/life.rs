02e5381e6e4dabc5c61189f5d0ff3109414fbc08
Adi-df <adidf-web@laposte.net>
2021-05-12 22:45:17 +0200

    Create Life System

--- src/systems/life.rs
+++ src/systems/life.rs
@@ -2,22 +2,30 @@ use std::sync::Arc;
 
 use hecs::{Entity, World};
 
-pub struct Life<T>
-where
-    T: Send + Sync + 'static + FnOnce(&mut World, &Entity),
-{
+pub struct Life {
     pub life: u16,
-    death: Arc<Box<T>>,
+    death: Arc<Box<dyn Send + Sync + 'static + Fn(&mut World, &Entity)>>,
 }
 
-impl<T> Life<T>
-where
-    T: Send + Sync + 'static + FnOnce(&mut World, &Entity),
-{
-    pub fn new(l: u16, d: Box<T>) -> Self {
+impl Life {
+    pub fn new(l: u16, d: Box<dyn Send + Sync + 'static + Fn(&mut World, &Entity)>) -> Self {
         Self {
             life: l,
             death: Arc::new(d),
         }
     }
+}
+
+pub fn life_system(world: &mut World) {
+    let died = world
+        .query_mut::<&Life>()
+        .into_iter()
+        .filter_map(|(e, l)| if l.life == 0 { Some(e) } else { None })
+        .collect::<Vec<Entity>>();
+
+    died.into_iter().for_each(|d| {
+        let l = &*world.query_one_mut::<&Life>(d).unwrap().death.clone();
+        l(world, &d);
+        world.despawn(d).unwrap();
+    })
 }

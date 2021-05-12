d5751786b377eb2fdfa6fe37ae537427295fc2e9
Adi-df <adidf-web@laposte.net>
2021-05-12 09:57:20 +0200

    Display Start text centered

--- src/mainmenu.rs
+++ src/mainmenu.rs
@@ -4,6 +4,17 @@ pub async fn menu() {
     loop {
         clear_background(BLACK);
 
+        // Text
+        let text = String::from("Press [SPACE] to start");
+        let measure = measure_text(&text, None, 30, 1.);
+        draw_text(
+            &text,
+            screen_width() / 2. - measure.width / 2.,
+            screen_height() / 2. - measure.height / 2.,
+            30.0,
+            WHITE,
+        );
+
         if is_key_pressed(KeyCode::Space) {
             break;
         }

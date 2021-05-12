b9572b2afdd6cdcff64679d7f64bede86b19e2f2
Adi-df <adidf-web@laposte.net>
2021-05-12 14:03:02 +0200

    Add speed parameter

--- src/systems/player_control.rs
+++ src/systems/player_control.rs
@@ -1,8 +1,12 @@
-pub struct PlayerControl(f32);
+use hecs::World;
+use macroquad::prelude::{KeyCode, is_key_down, screen_width};
+
+use super::physics::{Position, Size, Velocity};
+
+pub struct PlayerControl(pub f32);
 
 impl PlayerControl {
     pub fn new(speed: f32) -> Self {
         Self(speed)
     }
 }
-

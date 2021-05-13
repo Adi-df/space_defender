0a2a4c855387ebc1c9fcf2573b0d2e012bc5d008
Adrien Degliame <adidf-web@laposte.net>
2021-05-13 21:20:47 +0200

    Add Enemy Fire System module

--- src/systems.rs
+++ src/systems.rs
@@ -5,10 +5,10 @@ pub mod rect_renderer;
 
 // Others
 pub mod bullet;
+pub mod enemy_fire;
 pub mod fire_control;
 pub mod life;
 pub mod path_follower;
 pub mod physics;
 pub mod player_control;
 pub mod take_bullet_damage;
-pub mod enemy_fire;

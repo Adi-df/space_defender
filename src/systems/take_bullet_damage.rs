a6522075806d3d5b89dc6d42774ec9fb47aeafc3
Adi-df <adidf-web@laposte.net>
2021-05-20 13:14:22 +0200

    Add tag to collision system

--- src/systems/take_bullet_damage.rs
+++ src/systems/take_bullet_damage.rs
@@ -4,14 +4,13 @@ use hecs::{Entity, World};
 
 use super::{bullet, life, physics};
 
+type OnTouchCallback = Box<dyn Send + Sync + 'static + FnMut(&mut World, &Entity)>;
 #[derive(Clone)]
-pub struct TakeBulletDamage(
-    Arc<Mutex<Box<dyn Send + Sync + 'static + FnMut(&mut World, &Entity)>>>,
-);
+pub struct TakeBulletDamage(String, Arc<Mutex<OnTouchCallback>>);
 
 impl TakeBulletDamage {
-    pub fn new(on_touch: Box<dyn Send + Sync + 'static + FnMut(&mut World, &Entity)>) -> Self {
-        Self(Arc::new(Mutex::new(on_touch)))
+    pub fn new(tag: String, on_touch: OnTouchCallback) -> Self {
+        Self(tag.to_owned(), Arc::new(Mutex::new(on_touch)))
     }
 }
 
@@ -24,14 +23,14 @@ pub fn take_bullet_damage_system(world: &mut World) {
             &physics::Size,
         )>()
         .into_iter()
-        .map(|(e, (_, _, pos, size))| (e, pos.clone(), size.clone()))
-        .collect::<Vec<(Entity, physics::Position, physics::Size)>>();
+        .map(|(e, (tb, _, pos, size))| (e, tb.0.clone(), pos.clone(), size.clone()))
+        .collect::<Vec<(Entity, String, physics::Position, physics::Size)>>();
 
     let bullets = world
         .query_mut::<(&bullet::Bullet, &physics::Position, &physics::Size)>()
         .into_iter()
-        .map(|(e, (_, pos, size))| (e, pos.clone(), size.clone()))
-        .collect::<Vec<(Entity, physics::Position, physics::Size)>>();
+        .map(|(e, (b, pos, size))| (e, b.0.clone(), pos.clone(), size.clone()))
+        .collect::<Vec<(Entity, String, physics::Position, physics::Size)>>();
 
     let in_square = |p: (f32, f32), r: (f32, f32, f32, f32)| {
         r.0 <= p.0 && p.0 <= r.0 + r.2 && r.1 <= p.1 && p.1 <= r.1 + r.3
@@ -39,22 +38,26 @@ pub fn take_bullet_damage_system(world: &mut World) {
 
     let touched = targetable_entity
         .into_iter()
-        .filter_map(|(e, pos, size)| {
+        .filter_map(|(e, tag, pos, size)| {
             let touching = bullets
                 .iter()
-                .filter(|(_, bpos, bsize)| {
-                    in_square((bpos.0, bpos.1), (pos.0, pos.1, size.0, size.1))
+                .filter_map(|(_, btag, bpos, bsize)| {
+                    if in_square((bpos.0, bpos.1), (pos.0, pos.1, size.0, size.1))
                         || in_square((bpos.0 + bsize.0, bpos.1), (pos.0, pos.1, size.0, size.1))
                         || in_square((bpos.0, bpos.1 + bsize.1), (pos.0, pos.1, size.0, size.1))
                         || in_square(
                             (bpos.0 + bsize.0, bpos.1 + bsize.1),
                             (pos.0, pos.1, size.0, size.1),
-                        )
+                        ) && tag == *btag
+                    {
+                        Some(e)
+                    } else {
+                        None
+                    }
                 })
-                .map(|(e, _, _)| e.clone())
                 .collect::<Vec<Entity>>();
 
-            if touching.len() > 0 {
+            if !touching.is_empty() {
                 Some((e, touching))
             } else {
                 None
@@ -69,7 +72,7 @@ pub fn take_bullet_damage_system(world: &mut World) {
                     .query_one_mut::<(&TakeBulletDamage, &mut life::Life)>(s)
                     .unwrap();
                 me.1.life -= 1;
-                me.0 .0.clone()
+                me.0 .1.clone()
             };
             on_touch.lock().unwrap()(world, &s);
         }

00eb89b29e0d746580ae320f74da7c3fdddcc9cd
Adrien Degliame <adidf-web@laposte.net>
2021-05-20 18:56:40 +0200

    Refractor enemy facctory + 1rst Enemy type
    Change enemy generation and create zigzag enemy type

--- src/game.rs
+++ src/game.rs
@@ -1,9 +1,9 @@
 use std::{
-    ops::Range,
+    iter,
     sync::{Arc, Mutex},
 };
 
-use hecs::World;
+use hecs::{EntityBuilder, World};
 use macroquad::{prelude::*, rand::gen_range};
 
 use crate::exit_modes::ExitMode;
@@ -73,59 +73,66 @@ pub async fn game() -> ExitMode {
     ));
 
     let mut next_enemy: u16 = 30;
-    let new_enemy = |path_lenght: Range<u8>| {
-        let scorecounter_clone = scorecounter.clone();
-        let mut base_ennemy = (
-            animated_map_renderer::AnimatedMapRenderer::new(
-                vec![
-                    (vec![" ### ", "#@#@#", "#####", "&   &", " & & "].into(), 5),
-                    (vec![" ### ", "#@#@#", "#####", "&   &", "&   &"].into(), 5),
-                ],
-                vec![
-                    (' ', Color::from_rgba(0, 0, 0, 0)),
-                    ('#', RED),
-                    ('@', YELLOW),
-                    ('&', BLUE),
-                ]
-                .into(),
-            ),
-            life::Life::new(
-                1,
-                Box::new(move |_w, _e| {
-                    *scorecounter_clone.lock().unwrap() += 1;
-                }),
-            ),
-            take_bullet_damage::TakeBulletDamage::new(
-                String::from("Player Bullet"),
-                Box::new(move |_w, _e| {}),
-            ),
-            enemy_fire::EnemyFire::new(50..100),
-            physics::Size::new(35., 35.),
-            //Fixed
-            physics::Velocity::new(0., 0.),
-            // Dynamic
-            physics::Position::new(0., 0.),
-            path_follower::PathFollower::new(1., vec![]),
-        );
+    let new_enemy = || {
+        let mut types: Vec<Box<dyn FnMut() -> EntityBuilder>> = vec![Box::new(|| {
+            let mut enemy = EntityBuilder::new();
 
-        base_ennemy.6 = physics::Position::new(
-            gen_range(0., screen_width() - base_ennemy.4 .0),
-            gen_range(0., screen_height() / 3.),
-        );
-        base_ennemy.7 = path_follower::PathFollower::new(
-            gen_range(15., 30.),
-            (0..gen_range(path_lenght.start, path_lenght.end))
-                .into_iter()
-                .map(|_| {
-                    (
-                        gen_range(0., screen_width() - base_ennemy.4 .0),
-                        gen_range(0., screen_height() / 3. - base_ennemy.4 .1),
+            enemy
+                // Dynamic modified by Path
+                .add(physics::Position::new(20., 20.))
+                .add(physics::Velocity::new(0., 0.))
+                // Rendering
+                .add(physics::Size::new(35., 35.))
+                .add(animated_map_renderer::AnimatedMapRenderer::new(
+                    vec![
+                        (vec![" ### ", "#@#@#", "#####", "&   &", " & & "].into(), 5),
+                        (vec![" ### ", "#@#@#", "#####", "&   &", "&   &"].into(), 5),
+                    ],
+                    vec![
+                        (' ', Color::from_rgba(0, 0, 0, 0)),
+                        ('#', RED),
+                        ('@', YELLOW),
+                        ('&', BLUE),
+                    ]
+                    .into(),
+                ))
+                // Life & Fire
+                .add({
+                    let scorecounter_clone = scorecounter.clone();
+                    life::Life::new(
+                        1,
+                        Box::new(move |_w, _e| {
+                            *scorecounter_clone.lock().unwrap() += 1;
+                        }),
                     )
                 })
-                .collect(),
-        );
+                .add(take_bullet_damage::TakeBulletDamage::new(
+                    String::from("Player Bullet"),
+                    Box::new(move |_w, _e| {}),
+                ))
+                .add(enemy_fire::EnemyFire::new(20..100))
+                // Path
+                .add(path_follower::PathFollower::new(
+                    40.,
+                    iter::repeat(
+                        vec![
+                            (20., gen_range(0., screen_height() / 3.)),
+                            (
+                                screen_width() - 35. - 20.,
+                                gen_range(0., screen_height() / 3.),
+                            ),
+                        ]
+                        .into_iter(),
+                    )
+                    .take(4)
+                    .flatten()
+                    .collect(),
+                ));
+            enemy
+        })];
 
-        base_ennemy
+        let selected = gen_range(0, types.len());
+        types[selected]()
     };
 
     let score = loop {
@@ -143,7 +150,7 @@ pub async fn game() -> ExitMode {
         if next_enemy == 0 {
             next_enemy = gen_range(20u8, 100) as u16;
 
-            world.spawn(new_enemy(4..7));
+            world.spawn(new_enemy().build());
         }
 
         player_control::player_system(&mut world, &player);
